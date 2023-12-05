e787ba91749ca7abbeb0af514ec88eb06cc0bcf0
Jonas Marklén <txc@txc.se>
2023-12-05 11:14:34 +0100

    will i pass 6th stage

--- app/dns/common.py
+++ app/dns/common.py
@@ -352,26 +352,26 @@ class OpCode(EnumExtension):
 
 
 class ResponseCode(EnumExtension):
-    NO_ERROR = 0b0000
+    NO_ERROR = 0
     """No error condition"""
 
-    FORMAT_ERROR = 0b0001
+    FORMAT_ERROR = 1
     """Format error - The name server was unable to interpret the query."""
 
-    SERVER_FAILURE = 0b0010
+    SERVER_FAILURE = 2
     """Server failure - The name server was unable to process this query due
     to a problem with the name server."""
 
-    NAME_ERROR = 0b0011
+    NAME_ERROR = 3
     """Name Error - Meaningful only for responses from an authoritative name
     server, this code signifies that the domain name referenced in the query
     does not exist."""
 
-    NOT_IMPLEMENTED = 0b1000
+    NOT_IMPLEMENTED = 4
     """Not Implemented - The name server does not support the requested kind
     of query."""
 
-    REFUSED = 0b1001
+    REFUSED = 5
     """Refused - The name server refuses to perform the specified operation for
     policy reasons.  For example, a name server may not wish to provide the
     information to the particular requester, or a name server may not wish to
